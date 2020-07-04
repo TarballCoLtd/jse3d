6f535b0a0fec90505fcf836e47f365b675023bf9
Emery Ferrari <rubiquikyt@gmail.com>
2020-07-04 20:15:31 +0900

    Better example class

--- src/com/emeryferrari/jse3d/example/JSE3DExample.java
+++ src/com/emeryferrari/jse3d/example/JSE3DExample.java
@@ -4,15 +4,14 @@ import com.emeryferrari.jse3d.enums.*;
 import com.emeryferrari.jse3d.obj.*;
 public class JSE3DExample {
 	public static void main(String[] args) {
-		Object3D[] objects = new Object3D[1];
+		Object3D[] objects = new Object3D[23];
 		for (int i = 0; i < objects.length; i++) {
 			objects[i] = ObjectTemplate.getCube();
-			objects[i].movePosRel(new Vector3(i, i, i), new Vector3(0, 0, 0));
+			objects[i].movePosRel(new Vector3(i*2-objects.length/2, i*2-objects.length/2, i*2-objects.length/2), new Vector3(0, 0, 0));
 		}
 		Scene scene = new Scene(objects, 5.0);
 		Display display = new Display(scene, "jse3d demo", Math.toRadians(60), ObjectTemplate.getCube().points.length*objects.length, ObjectTemplate.getCube().points.length, objects.length);
 		display.enableFPSLogging();
-		display.disableFPSLimit();
 		display.enableCameraPositionPrinting();
 		display.setRenderTarget(RenderTarget.GPU);
 		display.startRender();
