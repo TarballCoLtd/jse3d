79e7d04a00f1c285061aa1668863b2662b087fe4
Emery Ferrari <rubiquikyt@gmail.com>
2020-07-07 13:10:11 +0900

    Line Javadocs

--- src/com/emeryferrari/jse3d/obj/Line.java
+++ src/com/emeryferrari/jse3d/obj/Line.java
@@ -8,11 +8,19 @@ public class Line implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public int pointID1;
 	public int pointID2;
+	/** Constructs a Line between two points.
+	 * @param pointID1 The first point's ID.
+	 * @param pointID2 The second point's ID.
+	 */
 	public Line(int pointID1, int pointID2) {
 		this.pointID1 = pointID1;
 		this.pointID2 = pointID2;
 	}
 	@Override
+	/** Compares the equality of this Line and another object.
+	 * @param object The object to which to compare this Line.
+	 * @return True if the two objects are equal.
+	 */
 	public boolean equals(Object object) {
 		if (object instanceof Line) {
 			Line temp = (Line) object;
