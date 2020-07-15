b4bba557ccb45681cb8999b45c61c98c0c1b7a4d
Emery Ferrari <rubiquikyt@gmail.com>
2020-07-15 20:17:49 +0900

    JSE3DConst Javadocs

--- src/com/emeryferrari/jse3d/JSE3DConst.java
+++ src/com/emeryferrari/jse3d/JSE3DConst.java
@@ -5,7 +5,13 @@ package com.emeryferrari.jse3d;
  */
 public class JSE3DConst {
 	private JSE3DConst() {}
+	/** Represents the name of this library.
+	 */
 	public static final String NAME = "jse3d";
+	/** Represents the version number of this build of the library.
+	 */
 	public static final String VERSION = "v2.5.7";
+	/** Represents the name of this library and the version of the current build concatenated with a space between.
+	 */
 	public static final String FULL_NAME = JSE3DConst.NAME + " " + JSE3DConst.VERSION;
 }
