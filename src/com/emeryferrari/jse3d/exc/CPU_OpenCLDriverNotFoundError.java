d9f3f861149dd1b542c427b568e473c6256f6b07
Emery Ferrari <rubiquikyt@gmail.com>
2020-07-15 20:13:04 +0900

    Exception Javadocs

--- src/com/emeryferrari/jse3d/exc/CPU_OpenCLDriverNotFoundError.java
+++ src/com/emeryferrari/jse3d/exc/CPU_OpenCLDriverNotFoundError.java
@@ -1,6 +1,12 @@
 package com.emeryferrari.jse3d.exc;
+/** Thrown if RenderTarget.CPU_MULTITHREADED is the current render target, but the CPU's OpenCL driver is not installed.
+ * @author Alyx Ferrari
+ * @since 2.0
+ */
 public class CPU_OpenCLDriverNotFoundError extends RuntimeException {
 	private static final long serialVersionUID = 1L;
+	/** Default constructor for CPU_OpenCLDriverNotFoundError.
+	 */
 	public CPU_OpenCLDriverNotFoundError() {
 		printStackTrace();
 		System.exit(17);
--- src/com/emeryferrari/jse3d/exc/ParticleNotSetException.java
+++ src/com/emeryferrari/jse3d/exc/ParticleNotSetException.java
@@ -1,7 +1,13 @@
 package com.emeryferrari.jse3d.exc;
+/** Thrown if the Particle of a Trajectory was accessed, but it was unassigned at runtime.
+ * @author Alyx Ferrari
+ * @since 2.5.7
+ */
 public class ParticleNotSetException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
+	/** Default constructor for ParticleNotSetException.
+	 */
 	public ParticleNotSetException() {
-		super("The particle of a Trajectory was accessed, but it was not set. Please assign the affected Trajectory to a Particle before calling the affected code.");
+		super("The Particle of a Trajectory was accessed, but it was not set. Please assign the affected Trajectory to a Particle before calling the affected code.");
 	}
 }
