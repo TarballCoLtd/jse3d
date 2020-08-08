8dbb3808d0a10d63d33d3e22cc385ee525195cd5
Alyx Ferrari <rubiquikyt@gmail.com>
2020-08-07 20:07:46 -0400

    Update lighting

--- src/com/alyxferrari/jse3d/example/DirectionalLightDemo.java
+++ src/com/alyxferrari/jse3d/example/DirectionalLightDemo.java
@@ -6,9 +6,9 @@ import java.awt.*;
 public class DirectionalLightDemo {
 	public static final String OBJ_PATH = "/Users/alyxferrari/Downloads/cat.obj"; // change this if you want to use this example
 	public static void main(String[] args) throws Exception {
-		Object3D[] objects = {Object3D.createFromObj(OBJ_PATH, new Color(255, 10, 10, 255))};
+		Object3D[] objects = {Object3D.createFromObj(OBJ_PATH, new Color(255, 20, 20, 255))};
 		Scene scene = new Scene(objects, 550);
-		DirectionalLight[] lights = {new DirectionalLight(new Vector3(0, 0, -1), 0.7f), new DirectionalLight(new Vector3(0, 0, 1), 0.4f)};
+		DirectionalLight[] lights = {new DirectionalLight(new Vector3(0, 0.8, -1), 0.7f), new DirectionalLight(new Vector3(0, -0.8, 1), 0.4f)};
 		scene.setDirectionalLights(lights);
 		Display display = new Display(scene, "jse3d obj model demo", Math.toRadians(60), objects[0].points.length, objects[0].points.length, 1);
 		display.disableFPSLimit();
