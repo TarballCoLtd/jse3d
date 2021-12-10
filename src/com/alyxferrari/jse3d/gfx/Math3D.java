be4410fe1fd11406df74f4bfe9c145c875ad86db
Alyx Ferrari <rubiquikyt@gmail.com>
2021-12-10 10:28:37 -0500

    Optimization of Math3D.hypot3

--- src/com/alyxferrari/jse3d/gfx/Math3D.java
+++ src/com/alyxferrari/jse3d/gfx/Math3D.java
@@ -8,7 +8,7 @@ public class Math3D {
 	 * @return The square root of x^2 plus y^2 plus z^2.
 	 */
 	public static final double hypot3(double x, double y, double z) {
-		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+		return Math.sqrt(x*x+y*y+z*z);
 	}
 	/** Equivalent to Math3D.hypot3(i,i,i).
 	 * @param i Value to use.
